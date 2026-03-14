deee305ef83e7086886d6bf53d27feb9b7cf6033
Emmanuel Merida <Emmanuel-Merida@outlook.com>
2026-03-13 23:16:36 -0600

    eliminar ip personal

--- src/main/java/com/ignis/prestamil/config/CorsConfig.java
+++ src/main/java/com/ignis/prestamil/config/CorsConfig.java
@@ -15,8 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-            "http://localhost:4200",
-            "http://192.168.50.15:4200"
+            "http://localhost:4200"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
